10bfa4cdf45a2c6679989dbc2d6e121f323bd1e5
siji1007 <christianjohnibanez@gmail.com>
2023-12-16 17:54:24 +0800

    CHANGE

--- BAND.py
+++ BAND.py
@@ -1,50 +1,64 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import ttkbootstrap as ttk
+from tkinter import Tk, Button,Entry,Frame
 
 class CustomApp:
     def __init__(self, master):
         self.master = master
         self.master.title("CNSC Marching Band")
-        self.master.geometry("1200x650")
+        self.master.geometry("800x700")
         self.master.resizable(False, False)
 
         self.create_widgets_background()
-
-        # Create ttkbootstrap styled widgets
         self.create_widgets_buttons()
-
         self.create_widgets_entry()
+        self.create_widgets_frames()
 
     def create_widgets_background(self):
-        # Load the background image
         original_image = Image.open("Dashboard.png")
-
-        # Resize the image while maintaining the aspect ratio
-        width, height = 1200, 650
+        width, height = 800, 700
         resized_image = original_image.resize((width, height), Image.LANCZOS)
-
-        # Convert the Image object to PhotoImage
         self.bg_image = ImageTk.PhotoImage(resized_image)
 
-        # Create a Canvas widget to place the background image
         self.canvas = tk.Canvas(self.master, width=width, height=height)
         self.canvas.pack(fill="both", expand=True)
-
-        # Place the resized background image on the Canvas
         self.canvas.create_image(0, 0, anchor="nw", image=self.bg_image)
 
     def create_widgets_buttons(self):
-        self.test_button = ttk.Button(self.canvas, text="hello", bootstyle="blue-outline")
-        self.test_button.pack(pady=10)
+        original_image = Image.open("FILTER.png")
+        resized_image = original_image.resize((60, 20), Image.LANCZOS)
+        self.search_image = ImageTk.PhotoImage(resized_image)
+
+        self.search_button = Button(self.canvas, image=self.search_image, bd=0, height=20, width=60, compound='center')
+        self.search_button.place(x=567, y=106)
+
+
+
 
     def create_widgets_entry(self):
-        # Create the entry widget with a custom background color
-        self.SEARCH_ENTRY = ttk.Entry(self.canvas, width=47, bootstyle="info")
-        self.SEARCH_ENTRY.place(x=520, y=100)
+        default_value = "Search"
 
-        # Configure the background color
-        self.SEARCH_ENTRY.configure(style="info.TEntry", background="maroon")
+        self.SEARCH_ENTRY = Entry(self.canvas, width=22, font=('Arial', 11, "bold"), bg="#993333",fg="white")
+        self.SEARCH_ENTRY.place(x=385, y=106)
+
+        # Set default value
+        self.SEARCH_ENTRY.insert(0, default_value)
+        self.SEARCH_ENTRY.bind("<FocusIn>", self.clear_default_value)
+
+
+    #FRAMES HERE
+    def create_widgets_frames(self):
+        self.FORM_1 = tk.Frame(self.master, bg="black")
+        self.FORM_1.place(x=29, y=185, width=300, height=185)
+
+        self.FORM_2 = tk.Frame(self.master, bg="black")
+        self.FORM_2.place(x=358, y=185, width=300, height=185)
+
+    def clear_default_value(self, event):
+        # Clear default value when the entry is clicked
+        if self.SEARCH_ENTRY.get() == "Search":
+            self.SEARCH_ENTRY.delete(0, tk.END)
 
     def run(self):
         self.master.mainloop()
